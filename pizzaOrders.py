9aa8219d5ce8b06b78776e648f166729f66e6500
grangerchristianschool <tech@grangerchristian.org>
2025-08-29 15:32:24 -0400

    Add files via upload

--- pizzaOrders.py
+++ pizzaOrders.py
@@ -110,9 +110,11 @@ def main():
 
     ### CHANGE FOR TESTING LOCALLY ###
     ordersheet = load_orders(sheet_id, worksheet_name)
+    ordersheet['A La Cart'] = np.where(ordersheet['A La Cart']=='','0',ordersheet['A La Cart'])
     st.dataframe(ordersheet)
+    st.write('x')
     st.write(ordersheet['A La Cart'].iloc[0])
-    st.write(type(ordersheet['A La Cart'].iloc[0]))
+    st.write('x')
     
     ###################################
     
