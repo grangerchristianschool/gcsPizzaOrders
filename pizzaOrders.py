989eac9107f20c772309b3edb41b26adaf31baff
grangerchristianschool <tech@grangerchristian.org>
2025-09-04 08:25:43 -0400

    Add files via upload

--- pizzaOrders.py
+++ pizzaOrders.py
@@ -192,7 +192,7 @@ def main():
         staff_orders = ordersheet[ordersheet['Grade']=='Staff']
         st.write(staff_orders.columns)
         #[['Parent Name','Confirm Order Date','Slices of Cheese','Slices of Pepperoni','Slices of Sausage','Meal Deal','A La Cart','How Will You Pay?']]
-        staff_orders.columns=['Person','Date','Cheese','Pepperoni','Sausage','Meal?','Cart','Payment']
+        #staff_orders.columns=['Person','Date','Cheese','Pepperoni','Sausage','Meal?','Cart','Payment']
         st.dataframe(staff_orders,hide_index=True)
 
 
